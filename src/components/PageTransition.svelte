5114c5a0c845f0d1e96c56d6f4a0af20f243e065
Tushar Date <tushardate@gmail.com>
2022-07-26 17:07:06 -0700

    Updated easings

--- src/components/PageTransition.svelte
+++ src/components/PageTransition.svelte
@@ -1,16 +1,25 @@
 <script type="ts">
 	import { fly } from 'svelte/transition';
-	import {backInOut} from 'svelte/easing'
+	import { quartOut, quartIn } from 'svelte/easing';
 	import { tweenIn, tweenOut } from '../utils/tweenMe';
+	import BezierEasing from 'bezier-easing';
 	export let url: URL;
 	const pageTransitionDuration = 500;
-	const easing = backInOut
+	const easing = quartOut;
+
+	// const ease = BezierEasing(0.35, 0, 0.2, 1)
+	const customEase = BezierEasing(0, 0, 1, 1);
 </script>
 
 {#key url}
 	<div
-		in:fly={{ y: 15, duration: pageTransitionDuration, easing: easing, delay: pageTransitionDuration }}
-		out:fly={{ y: -15, easing: easing, duration: pageTransitionDuration }}
+		in:fly={{
+			y: 15,
+			duration: pageTransitionDuration,
+			easing: quartOut,
+			delay: pageTransitionDuration
+		}}
+		out:fly={{ y: 15, easing: quartIn, duration: pageTransitionDuration }}
 	>
 		<slot />
 	</div>
