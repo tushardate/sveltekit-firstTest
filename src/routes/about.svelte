d5e64ac5cc7e018445dd107393030c08364b407c
Tushar Date <tushardate@gmail.com>
2023-03-16 22:19:09 -0700

    Updated about page locally and pushed to github.

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -3,4 +3,4 @@
 	import '../app.scss';
 </script>
 
-<div>About page has changed</div>
+<div>About page has changed. We made some changes on the local git on my computer. Now comitting it to git hub</div>
